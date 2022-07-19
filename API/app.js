79371214147f6b343f179cffc81656862efcf8ac
aymkh <aymen4real@gmail.com>
2022-07-19 18:40:15 +0100

    port typo

--- API/app.js
+++ API/app.js
@@ -10,7 +10,7 @@ var nameRoutes = require('./routes/names');
 var app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:40801");
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
